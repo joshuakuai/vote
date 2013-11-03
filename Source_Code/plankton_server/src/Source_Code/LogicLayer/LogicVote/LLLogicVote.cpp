24a307a4c055c6a3d097008aa7fc6790a0eabab2
joshuakuai <bearworldbrave@gmail.com>
2013-11-03 18:46:35 -0500

    Add Logic file

---
+++ Source_Code/plankton_server/src/Source_Code/LogicLayer/LogicVote/LLLogicVote.cpp
@@ -0,0 +1,18 @@
+/*
+ * LLLogicVote.cpp
+ *
+ *  Created on: Nov 3, 2013
+ *      Author: kuaijianghua
+ */
+
+#include "LLLogicVote.h"
+
+LLLogicVote::LLLogicVote() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LLLogicVote::~LLLogicVote() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Source_Code/plankton_server/src/Source_Code/LogicLayer/LogicVote/LLLogicVote.h
@@ -0,0 +1,17 @@
+/*
+ * LLLogicVote.h
+ *
+ *  Created on: Nov 3, 2013
+ *      Author: kuaijianghua
+ */
+
+#ifndef LLLOGICVOTE_H_
+#define LLLOGICVOTE_H_
+
+class LLLogicVote {
+public:
+	LLLogicVote();
+	virtual ~LLLogicVote();
+};
+
+#endif /* LLLOGICVOTE_H_ */
